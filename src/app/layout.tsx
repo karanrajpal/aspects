ff4fd4d1f6f1b7d109f5075e339f0d8a59f3ede0
Karan Rajpal <amigokaran@gmail.com>
2024-09-28 14:14:07 -0400

    Initialized PWA Next app:

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { PWAWrapper } from "@/components/pwa-wrapper";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Aspects",
+    description: "Bird's eye view of the different aspects of your life",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <PWAWrapper>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </PWAWrapper>
+            </body>
+        </html>
+    );
+}
